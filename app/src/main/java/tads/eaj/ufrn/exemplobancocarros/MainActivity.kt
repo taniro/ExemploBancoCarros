0f8cfc8ec4839101574016880db76bf348850f72
Taniro <tanirocr@gmail.com>
2022-10-06 10:42:18 -0300

    Initial commit

---
+++ app/src/main/java/tads/eaj/ufrn/exemplobancocarros/MainActivity.kt
@@ -0,0 +1,11 @@
+package tads.eaj.ufrn.exemplobancocarros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
